f263bae1bff40d2deae20a177485099bae643010
diplg <dimas4354@icloud.com>
2022-04-21 22:45:56 +0300

    первое задание

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+Console.Write("Число равняется: (");
+int number = new Random(). Next(100 , 999);
+Console.Write($"{number} -->");
+
+int number1 = number / 100;
+int number2 = number % 10;
+Console.WriteLine($"число без второй цифры = {number1}{number2}");
