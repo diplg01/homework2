a99ec3ae9d2fd2bf5f6815b7545fed710933997d
diplg <dimas4354@icloud.com>
2022-04-21 23:18:57 +0300

    третье задание

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите число от 1 до 7; ");
+int nam = Convert.ToInt32(Console.ReadLine());
+if (nam > 7) 
+{
+    Console.WriteLine(" В неделе 7 дней");
+}
+if(1 == nam) Console.Write(" Понеделик - будний день ");
+if(2 == nam) Console.Write(" Вторник - будний день ");
+if(3 == nam) Console.Write(" Среда - будний день ");
+if(4 == nam) Console.Write(" Четверг - будний день ");
+if(5 == nam) Console.Write(" Пятница - будний день "); 
+if(6 == nam) Console.Write(" суббота - выходной день ");
+if(7 == nam) Console.Write(" воскресенье - выходной день ");
