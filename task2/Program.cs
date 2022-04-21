0073276ec9151ebfce07da738e29d2b9d873d1cd
diplg <dimas4354@icloud.com>
2022-04-21 22:47:00 +0300

    второе задание

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Число равняется: (");
+int number = new Random(). Next(1 , 300);
+Console.Write(number);
+if (number < 100)
+{
+    Console.WriteLine(") Третьей цифры нет!!!");
+}
+else
+{
+    Console.WriteLine($") Третья цифра числа {number % 10}");
+}
